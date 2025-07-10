ac980d82c1ba1a8d3fe6611bdda485ff97a94e72
Richard Ambrosio <richard.ambrosio1@live.com>
2025-07-10 13:48:08 -0300

    Feat: add create questions, create room and get room questions

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import {
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -22,5 +25,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestionsRoute)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT })
